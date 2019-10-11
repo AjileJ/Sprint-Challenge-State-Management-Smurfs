b7e09313f399b6a03e42276ef8ba395241cfe0d4
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-10-11 10:02:34 -0700

    finished mvp to my understanding

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SmurfInfo from './SmurfInfo';
+import FormikLoginForm from './LoginForm';
 import "./App.css";
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <SmurfInfo/>
+        <FormikLoginForm />
       </div>
     );
   }
--- smurfs/src/components/LoginForm.js
+++ smurfs/src/components/LoginForm.js
@@ -5,5 +5,57 @@ import * as Yup from 'yup';
 import axios from 'axios';
 
 function LoginForm({ values,errors,touched, isSubmitting }){
-  
+  return (
+    <Form>
+      <div>
+        {touched.name && errors.name && <p>{errors.name}</p>}
+        <Field type='text' name='name' placeholder='Name'/>
+      </div>
+      <div>
+        {touched.age && errors.age && <p>{errors.age}</p>}
+        <Field type='text' name='age' placeholder='Age'/>
+      </div>
+      <div>
+        {touched.height && errors.height && <p>{errors.height}</p>}
+        <Field type='text' name='height' placeholder='Height'/>
+      </div>
+      <button disabled = {isSubmitting}>Submit</button>
+    </Form>
+  );
 }
+
+const FormikLoginForm = withFormik({
+  mapPropsToValues({name, age, height}){
+    return {
+      name: name || '',
+      age: age || '',
+      height: height || ''
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+    .required('Name is required'),
+    age: Yup.string()
+    .required('Age is required'),
+    height: Yup.string()
+    .required('Height is required')
+  }),
+  handleSubmit(values, {resetForm, setErrors, setSubmitting}) {
+    if (values.name === "Brainey") {
+      setErrors({name: "That Name is already taken"});
+    }else {
+      axios
+      .post('http://localhost:3333/smurfs', values)
+      .then(res => {
+        console.log(res);
+        resetForm();
+        setSubmitting(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setSubmitting(false);
+      });
+    }
+  }
+})(LoginForm);
+export default FormikLoginForm;
